7c11884507cd3863d7edfc37e2ecde253ccce8fb
Gregory Anne <ganne@effisoft-group.com>
2020-02-19 17:21:53 +0100

    clear

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculateur-conges-angular';
+  startDate = new Date(2019, 9, 1);
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatMomentDateModule} from '@angular/material-moment-adapter';
+import {MatInputModule} from '@angular/material/input';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const modules = [
+  BrowserModule,
+  BrowserAnimationsModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatMomentDateModule,
+  MatInputModule
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [...modules],
+  exports: [...modules],
   providers: [],
   bootstrap: [AppComponent]
 })
